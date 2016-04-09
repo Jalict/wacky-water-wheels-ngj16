d34ee5a4dca71f09a9c1b7732bcbd857d8ed4ef9
Maja Daniela Hansen <MajaDanielaHansen@Gmail.com>
2016-04-09 18:00:13 +0200

    Basic camera movement

---
+++ Assets/Scripts/tempMovement.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class tempMovement : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+			if (Input.GetKey (KeyCode.A)) {
+			transform.Translate (-10f * Time.deltaTime, 0f, 0f);
+				}
+		if (Input.GetKey (KeyCode.D)) {
+			transform.Translate (10f * Time.deltaTime, 0f, 0f);
+				}
+		if (Input.GetKey (KeyCode.W)) {
+			transform.Translate (0f, 0f, 5f * Time.deltaTime);
+				}
+
+		if (Input.GetKey (KeyCode.S)) {
+			transform.Translate (0f, 0f, -5f * Time.deltaTime);
+
+				}
+
+	}
+}
