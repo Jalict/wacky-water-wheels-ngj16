20ac065b5e1785cf2871d16dad98d26d27d3511a
Hethix <An.ryge@gmail.com>
2016-04-09 17:58:43 +0200

    Added tilting water

--- Assets/Scripts/LeakingWater.cs
+++ Assets/Scripts/LeakingWater.cs
@@ -3,25 +3,30 @@ using System.Collections;
 
 public class LeakingWater : MonoBehaviour {
 
-	private float fullTruck = 38f;
-	public float currentWaterAmount = 38f;
-	private float emptyTruck = -34f;
+	private float fullTruck = 43f;
+	public float currentWaterAmount = 43f;
+	private float emptyTruck = -38f;
 	public GameObject waterPlane;
+	private float waterRotation;
 
 	// Use this for initialization
 	void Start () {
+		waterRotation = transform.localRotation.z;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		Debug.Log(transform.localRotation.x * Mathf.Rad2Deg);
 		if(currentWaterAmount > emptyTruck){
-			if(Mathf.Abs(transform.localRotation.x * Mathf.Rad2Deg) > 5.0f ) { 
+			if(Mathf.Abs(transform.localRotation.z * Mathf.Rad2Deg) > 2.0f) { 
 				Vector3 v = waterPlane.transform.localPosition;
-				currentWaterAmount -= Mathf.Abs(transform.localRotation.x * Mathf.Rad2Deg)/50;
+				currentWaterAmount -= Mathf.Abs(transform.localRotation.z * Mathf.Rad2Deg)/50;
 				v.y = currentWaterAmount;
 				waterPlane.transform.localPosition = v;
 			}
 		}
+		Debug.Log(transform.localRotation.z * Mathf.Rad2Deg);
+
+		waterPlane.transform.Rotate(Vector3.forward, (waterRotation - transform.localRotation.z)*200);
+		waterRotation = transform.localRotation.z;
 	}
 }
